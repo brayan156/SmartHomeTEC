a8b8f18b2b7ff46da62036bc583a101d04a2019d
Tomás <tomas.seguram@gmail.com>
2021-05-05 00:33:08 -0600

    updates

--- FrontEnd/src/app/Administrador/dashboard/dashboard.component.ts
+++ FrontEnd/src/app/Administrador/dashboard/dashboard.component.ts
@@ -31,6 +31,31 @@ export class DashboardComponent implements OnInit {
 
   }
 
+  chart2 = {
+    title: '',
+    type: 'GeoChart',
+    data: [
+      ["China", "China: 3k"],
+      ["India", "India: 1,242,620,000"],
+      ["Costa Rica", "Costa Rica: 2k"],
+      ["US", "US: 317,842,000"],
+      ["Indonesia", "Indonesia: 247,424,598"],
+      ["Brazil", "Brazil: 201,032,714"],
+      ["Pakistan", "Pakistan: 186,134,000"],
+      ["Nigeria", "Nigeria: 173,615,000"],
+      ["Bangladesh", "Bangladesh: 152,518,015"],
+      ["Russia", "Russia: 146,019,512"],
+      ["Japan", "Japan: 127,120,000"]
+    ],
+    columnNames: ["Country", "Population"],
+    options: {
+      showTip: true
+    },
+    width: 550,
+    height: 400,
+
+  };
+
   
 
   
