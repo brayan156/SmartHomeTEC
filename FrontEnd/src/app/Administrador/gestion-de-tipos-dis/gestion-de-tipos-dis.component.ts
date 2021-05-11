f9e45eeb2912d0805dd91f7671ba8ae06d209247
Brayan <brayanleon25@hotmail.com>
2021-05-10 20:18:20 -0600

    Merge remote-tracking branch 'origin/front_end' into backend

--- FrontEnd/src/app/Administrador/gestion-de-tipos-dis/gestion-de-tipos-dis.component.ts
+++ FrontEnd/src/app/Administrador/gestion-de-tipos-dis/gestion-de-tipos-dis.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Tipo} from '../../Comunicacion/tipo';
 import {ServiciosService} from '../../servicios.service';
+import {DispositivoModelo} from '../../Comunicacion/dispositivo-modelo';
 
 @Component({
   selector: 'app-gestion-de-tipos-dis',
@@ -10,18 +11,53 @@ import {ServiciosService} from '../../servicios.service';
 export class GestionDeTiposDisComponent implements OnInit {
   constructor(private service: ServiciosService) { }
 
-  tipo: Tipo = new Tipo();
+  // tslint:disable-next-line:new-parens
+  tipo: Tipo = new Tipo;
   listaDeTipos: Tipo[] = [];
+  // tslint:disable-next-line:new-parens
+  tipoActual: Tipo = new Tipo;
+  listaDeModelos: DispositivoModelo[] = [];
+  oldDispositivocreado: DispositivoModelo = new DispositivoModelo;
 
   ngOnInit(): void {
     this.service.getTiposDispositivos().subscribe(lista => {
       this.listaDeTipos = lista;
       console.log(this.listaDeTipos);
     });
+    this.service.obtenerDispositivosModelo().subscribe(lista => this.listaDeModelos = lista);
   }
 
-  public crearDipositivo(tipo: Tipo): void{
+  public crearTipoDipositivo(tipo: Tipo): void{
     console.log(tipo);
     this.service.crearTipoDispositivo(tipo).subscribe(a => console.log(a));
+    this.ngOnInit();
+
+  }
+  public editarTipoDipositivo(tipo: Tipo): void{
+    this.service.editarTipoDispositivo(tipo.nombre, tipo).subscribe(a => {console.log(a);
+                                                                          this.ngOnInit();
+    });
+  }
+
+  public eliminarTipoDispositivo(): void{
+    this.service.eliminarTipoDispositivo(this.tipoActual.nombre).subscribe(a => {console.log(a);
+                                                                                 this.ngOnInit();
+    });
+  }
+
+  public tipoActualF(tipo: Tipo): void{
+    this.tipoActual = tipo;
+  }
+
+  public obtenerInformacionItem(dipositivo: DispositivoModelo): void{
+    this.oldDispositivocreado = dipositivo;
+    this.ngOnInit();
+  }
+
+  public editarDispositivo(dispositivoModelo: DispositivoModelo): void{
+    console.log(dispositivoModelo);
+    this.service.editarDipositivoModelo(dispositivoModelo.modelo, dispositivoModelo).subscribe(a => console.log(a));
+    this.ngOnInit();
   }
 }
+
